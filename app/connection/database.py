b1f728657bd76433f3575316b80c07637a8fa09a
rsarellano <rjsarellano@gmail.com>
2025-06-13 00:26:58 +0800

    completed working create property api

--- app/connection/database.py
+++ app/connection/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-URL_DATABASE = "postgresql://postgres:admin123$@localhost:5432/aqqire"
+URL_DATABASE = "postgresql://postgres:admin1234@localhost:5432/aqqire"
 
 
 

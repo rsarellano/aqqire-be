a198005d20b16776f6d0eb4c5c3f338e432d4adf
rsarellano <rjsarellano@gmail.com>
2025-06-13 18:57:37 +0800

    added working controllers for property

--- app/controllers/properties.py
+++ app/controllers/properties.py
@@ -1,3 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from schemas.propertySchema.propertySchema import PropertyBase
-from services.propertyService.properties import 
+from app.schemas.propertySchema import PropertyBase, PropertyCreate
+from app.services.propertyService import get_all_properties, create_property
+
+
--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 from typing import Union, List, Annotated
 from pydantic import BaseModel
 from fastapi import FastAPI, Depends, status
-
-from app.services.propertyService import get_all_properties, create_property
-
 from app.connection.database import engine,sessionLocal, Base
 from sqlalchemy.orm import Session
+
 from app.schemas.propertySchema import PropertyCreate, PropertyBase
 from app.models.properties import Property
+from app.services.propertyService import get_all_properties, create_property
 
 Base.metadata.create_all(bind=engine)
 

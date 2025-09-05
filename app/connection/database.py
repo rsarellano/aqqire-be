ab1878a5c1bc3538cd75b73329307cc8d35863f5
rjsarellano <rjsarellano@gmail.com>
2025-09-05 23:41:05 +0800

    made database connection async

--- app/connection/database.py
+++ app/connection/database.py
@@ -1,26 +1,29 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import async_sessionmaker
+
 from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
 
-URL_DATABASE = "postgresql://postgres:admin123$@localhost:5432/aqqire"
+URL_DATABASE = "postgresql+asyncpg://postgres:admin123$@localhost:5432/aqqire"
 
 
 
-engine = create_engine(URL_DATABASE)
+engine = create_async_engine(URL_DATABASE)
 
-sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+sessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 
-def get_db():
-    db = sessionLocal()
+async def get_db():
+    db = await sessionLocal()
     try:
         yield db
     finally:
-        db.close()
+        await db.close()
 
 
         

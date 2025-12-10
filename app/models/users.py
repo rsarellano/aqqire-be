2815d28258c283c925057e9921f548531c84be52
rsarellano <rjsarellano@gmail.com>
2025-12-11 01:30:57 +0800

    updated commit for new repo

--- app/models/users.py
+++ app/models/users.py
@@ -7,7 +7,18 @@ from sqlalchemy.dialects.postgresql import UUID
 class Users(Base):
     __tablename__ = "users"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    _id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    active_token = Column(String, index=True, nullable=True)
+    active = Column(Boolean, index=True, nullable=False, default=True)
+    can_post = Column(Boolean, index=True, nullable=False, default=True)
+    city = Column(String, index=True, nullable=True)
+    click_report_instruction = Column(Boolean, index=True, nullable=True, default=False)
+    college_accomplishment = Column(String, index=True, nullable=True)
+    
+    
+    
+    
+    
     user_email = Column(String, index=True, nullable=False)
     user_firstName = Column( String, index=True, nullable=False)
     user_lastName = Column (String, index=True, nullable=False)
